e9fcb565f63e127cb15342635d79e65a77ebe411
garciadaniela <garciadanielajb2@gmail.com>
2021-04-01 21:05:21 -0300

    cambios jueves

---
+++ login/acceso-confirmado.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-12 py-3">
+            <h1>Bienvenido al sistema</h1>
+        </div>
+        <div class="alert alert-primary my-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ipsum, aliquam sit error eveniet repellendus accusamus. Voluptas possimus odit inventore natus quisquam debitis. Velit vero, magni qui mollitia enim rerum?</p>
+        </div>
+        <div class="col-2">
+            <a href="index.php" class="btn btn-primary" style="color: #fff;">Salir</a>
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
